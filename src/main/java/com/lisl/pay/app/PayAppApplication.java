6c3ff0f4362b7c28cb315b023f84793b7105c223
MrLiCTO <javaeemrli@163.com>
2017-01-20 17:16:43 +0800

    Initial commit

---
+++ src/main/java/com/lisl/pay/app/PayAppApplication.java
@@ -0,0 +1,12 @@
+package com.lisl.pay.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/lisl/pay/app/Service/PayService.java
@@ -0,0 +1,8 @@
+package com.lisl.pay.app.Service;
+
+/**
+ * Created by Administrator on 2017/1/19.
+ */
+public interface PayService {
+    //public String order_qr();
+}
