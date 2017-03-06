ea337e1d307b346ee669a296f3515992365ab20d
MrLiCTO <javaeemrli@163.com>
2017-03-06 19:46:12 +0800

    继续整合spring security

---
+++ src/main/java/com/lisl/pay/app/security/handler/MySimpleUrlLogoutSuccessHandler.java
@@ -0,0 +1,19 @@
+package com.lisl.pay.app.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Mr_Li on 2017/3/6.
+ */
+public class MySimpleUrlLogoutSuccessHandler extends SimpleUrlLogoutSuccessHandler {
+    @Override
+    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        super.onLogoutSuccess(request, response, authentication);
+    }
+}
